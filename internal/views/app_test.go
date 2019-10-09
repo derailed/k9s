196884a9a8158f699985b2c8cba99e55149d5db4
derailed <fernand.galiana@gmail.com>
2019-10-08 21:21:27 -0700

    update test

--- internal/views/app_test.go
+++ internal/views/app_test.go
@@ -11,6 +11,6 @@ func TestNewApp(t *testing.T) {
 	a := NewApp(config.NewConfig(ks{}))
 	a.Init("blee", 10)
 
-	assert.Equal(t, 10, len(a.GetActions()))
+	assert.Equal(t, 11, len(a.GetActions()))
 	assert.Equal(t, false, a.HasSkins)
 }
