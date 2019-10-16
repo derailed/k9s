add9c42c217e50c0ed946c92409e4b7052006287
Bruno <bruno@stilingue.com.br>
2019-10-16 19:41:15 -0300

    Printing simplified version (ansible friendly)
    solving #355

--- cmd/version.go
+++ cmd/version.go
@@ -8,26 +8,38 @@ import (
 )
 
 func versionCmd() *cobra.Command {
-	return &cobra.Command{
+	var silent bool
+
+	command := cobra.Command{
 		Use:   "version",
 		Short: "Print version/build info",
 		Long:  "Print version/build information",
 		Run: func(cmd *cobra.Command, args []string) {
-			printVersion()
+			printVersion(silent)
 		},
 	}
+
+	command.PersistentFlags().BoolVarP(&silent, "short", "s", false, "Simplified print version for resumed printing")
+
+	return &command
 }
 
-func printVersion() {
+func printVersion(silent bool) {
 	const secFmt = "%-10s "
+	var outputColor color.Paint
 
-	printLogo(color.Cyan)
-	printTuple(secFmt, "Version", version)
-	printTuple(secFmt, "Commit", commit)
-	printTuple(secFmt, "Date", date)
+	if silent {
+		outputColor = color.White
+	} else {
+		outputColor = color.Cyan
+		printLogo(outputColor)
+	}
+	printTuple(secFmt, "Version", version, outputColor)
+	printTuple(secFmt, "Commit", commit, outputColor)
+	printTuple(secFmt, "Date", date, outputColor)
 }
 
-func printTuple(format, section, value string) {
-	fmt.Printf(color.Colorize(fmt.Sprintf(format, section+":"), color.Cyan))
+func printTuple(format, section, value string, outputColor color.Paint) {
+	fmt.Printf(color.Colorize(fmt.Sprintf(format, section+":"), outputColor))
 	fmt.Println(color.Colorize(value, color.White))
 }
