32a53e589951625f02743ea5c2da9197550e7282
Richard Hull <rm_hull@yahoo.co.uk>
2022-09-23 23:53:15 +0100

    Add a closing tag when showing timestamp in log view

--- internal/dao/log_item.go
+++ internal/dao/log_item.go
@@ -73,6 +73,7 @@ func (l *LogItem) Render(paint string, showTime bool, bb *bytes.Buffer) {
 		for i := len(l.Bytes[:index]); i < 30; i++ {
 			bb.WriteByte(' ')
 		}
+		bb.WriteString("[-::]")
 	}
 
 	if l.Pod != "" {
