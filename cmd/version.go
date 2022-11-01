143fadf59945425c89386026f9777eb85922152f
wkyoshida <wkyoshida.dev@gmail.com>
2022-11-01 11:27:29 -0300

    fix(cmd): use default terminal color for cmd output

--- cmd/version.go
+++ cmd/version.go
@@ -41,7 +41,7 @@ func printVersion(short bool) {
 
 func printTuple(fmat, section, value string, outputColor color.Paint) {
 	if outputColor != -1 {
-		fmt.Fprintf(out, fmat, color.Colorize(section+":", outputColor), color.Colorize(value, color.LightGray))
+		fmt.Fprintf(out, fmat, color.Colorize(section+":", outputColor), value)
 		return
 	}
 	fmt.Fprintf(out, fmat, section, value)
