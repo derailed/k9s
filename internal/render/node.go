derailed/k9s

commit 344990e096ce5173dbdff7fe140d0078afe2d9b1

Pod view resembles more the output of kubectl get pods -o wide (#2125)

internal/render/pod.go
@@ -80,8 +80,12 @@ func (Pod) Header(ns string) Header {
 		HeaderColumn{Name: "NAME"},
 		HeaderColumn{Name: "PF"},
 		HeaderColumn{Name: "READY"},
-		HeaderColumn{Name: "RESTARTS", Align: tview.AlignRight},
 		HeaderColumn{Name: "STATUS"},
+		HeaderColumn{Name: "RESTARTS", Align: tview.AlignRight},
+		HeaderColumn{Name: "IP"},
+		HeaderColumn{Name: "NODE"},
+		HeaderColumn{Name: "NOMINATED NODE", Wide: true},
+		HeaderColumn{Name: "READINESS GATES", Wide: true},
 		HeaderColumn{Name: "CPU", Align: tview.AlignRight, MX: true},
 		HeaderColumn{Name: "MEM", Align: tview.AlignRight, MX: true},
 		HeaderColumn{Name: "CPU/R:L", Align: tview.AlignRight, Wide: true},
@@ -90,13 +94,9 @@ func (Pod) Header(ns string) Header {
 		HeaderColumn{Name: "%CPU/L", Align: tview.AlignRight, MX: true},
 		HeaderColumn{Name: "%MEM/R", Align: tview.AlignRight, MX: true},
 		HeaderColumn{Name: "%MEM/L", Align: tview.AlignRight, MX: true},
-		HeaderColumn{Name: "IP"},
-		HeaderColumn{Name: "NODE"},
 		HeaderColumn{Name: "QOS", Wide: true},
 		HeaderColumn{Name: "LABELS", Wide: true},
 		HeaderColumn{Name: "VALID", Wide: true},
-		HeaderColumn{Name: "NOMINATED NODE", Wide: true},
-		HeaderColumn{Name: "READINESS GATES", Wide: true},
 		HeaderColumn{Name: "AGE", Time: true},
 	}
 }
@@ -126,6 +126,11 @@ func (p Pod) Render(o interface{}, ns string, row *Row) error {
 		strconv.Itoa(cr) + "/" + strconv.Itoa(len(po.Spec.Containers)),
 		strconv.Itoa(rc),
 		phase,
+		strconv.Itoa(rc),
+		na(po.Status.PodIP),
+		na(po.Spec.NodeName),
+		asNominated(po.Status.NominatedNodeName),
+		asReadinessGate(po),
 		toMc(c.cpu),
 		toMi(c.mem),
 		toMc(r.cpu) + ":" + toMc(r.lcpu),
@@ -134,13 +139,9 @@ func (p Pod) Render(o interface{}, ns string, row *Row) error {
 		client.ToPercentageStr(c.cpu, r.lcpu),
 		client.ToPercentageStr(c.mem, r.mem),
 		client.ToPercentageStr(c.mem, r.lmem),
-		na(po.Status.PodIP),
-		na(po.Spec.NodeName),
 		p.mapQOS(po.Status.QOSClass),
 		mapToStr(po.Labels),
 		asStatus(p.diagnose(phase, cr, len(ss))),
-		asNominated(po.Status.NominatedNodeName),
-		asReadinessGate(po),
 		toAge(po.GetCreationTimestamp()),
 	}
 

internal/model/table_int_test.go
@@ -105,7 +105,7 @@ func TestTableHydrate(t *testing.T) {
 
 	assert.Nil(t, hydrate("blee", oo, rr, render.Pod{}))
 	assert.Equal(t, 1, len(rr))
-	assert.Equal(t, 22, len(rr[0].Fields))
+	assert.Equal(t, 23, len(rr[0].Fields))
 }
 
 func TestTableGenericHydrate(t *testing.T) {

internal/render/pod_test.go
@@ -159,7 +159,7 @@ func TestPodRender(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "default/nginx", r.ID)
-	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Running", "100", "50", "100:0", "70:170", "100", "n/a", "71", "29", "172.17.0.6", "minikube", "BE"}
+	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Running", "0", "172.17.0.6", "minikube", "<none>", "<none>", "100", "50", "100:0", "70:170", "100", "n/a"}
 	assert.Equal(t, e, r.Fields[:17])
 }
 
@@ -190,7 +190,7 @@ func TestPodInitRender(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "default/nginx", r.ID)
-	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Init:0/1", "10", "10", "100:0", "70:170", "10", "n/a", "14", "5", "172.17.0.6", "minikube", "BE"}
+	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Init:0/1", "0", "172.17.0.6", "minikube", "<none>", "<none>", "10", "10", "100:0", "70:170", "10", "n/a"}
 	assert.Equal(t, e, r.Fields[:17])
 }
 

commit 3848319ae635e201513d8d1579b14854c1f30f62

Clean up issues introduced by #2125

internal/render/node.go
@@ -53,11 +53,11 @@ func (Node) Header(_ string) Header {
 func (n Node) Render(o interface{}, ns string, r *Row) error {
 	oo, ok := o.(*NodeWithMetrics)
 	if !ok {
-		return fmt.Errorf("Expected *NodeAndMetrics, but got %T", o)
+		return fmt.Errorf("expected *NodeAndMetrics, but got %T", o)
 	}
 	meta, ok := oo.Raw.Object["metadata"].(map[string]interface{})
 	if !ok {
-		return fmt.Errorf("Unable to extract meta")
+		return fmt.Errorf("unable to extract meta")
 	}
 	na := extractMetaField(meta, "name")
 	var no v1.Node

internal/render/pod.go
@@ -124,7 +124,6 @@ func (p Pod) Render(o interface{}, ns string, row *Row) error {
 		po.ObjectMeta.Name,
 		"●",
 		strconv.Itoa(cr) + "/" + strconv.Itoa(len(po.Spec.Containers)),
-		strconv.Itoa(rc),
 		phase,
 		strconv.Itoa(rc),
 		na(po.Status.PodIP),

internal/model/table_int_test.go
@@ -105,7 +105,7 @@ func TestTableHydrate(t *testing.T) {
 
 	assert.Nil(t, hydrate("blee", oo, rr, render.Pod{}))
 	assert.Equal(t, 1, len(rr))
-	assert.Equal(t, 23, len(rr[0].Fields))
+	assert.Equal(t, 22, len(rr[0].Fields))
 }
 
 func TestTableGenericHydrate(t *testing.T) {

internal/render/pod_test.go
@@ -159,7 +159,7 @@ func TestPodRender(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "default/nginx", r.ID)
-	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Running", "0", "172.17.0.6", "minikube", "<none>", "<none>", "100", "50", "100:0", "70:170", "100", "n/a"}
+	e := render.Fields{"default", "nginx", "●", "1/1", "Running", "0", "172.17.0.6", "minikube", "<none>", "<none>", "100", "50", "100:0", "70:170", "100", "n/a", "71"}
 	assert.Equal(t, e, r.Fields[:17])
 }
 
@@ -190,7 +190,7 @@ func TestPodInitRender(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "default/nginx", r.ID)
-	e := render.Fields{"default", "nginx", "●", "1/1", "0", "Init:0/1", "0", "172.17.0.6", "minikube", "<none>", "<none>", "10", "10", "100:0", "70:170", "10", "n/a"}
+	e := render.Fields{"default", "nginx", "●", "1/1", "Init:0/1", "0", "172.17.0.6", "minikube", "<none>", "<none>", "10", "10", "100:0", "70:170", "10", "n/a", "14"}
 	assert.Equal(t, e, r.Fields[:17])
 }
 