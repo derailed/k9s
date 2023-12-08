e3ee77ff118af50ed4200df30c486b692cd37a18
Arthur Coelho <arthur.cavalcante.puc@gmail.com>
2023-12-08 01:33:04 -0300

    feat: paint hpa line when replicas >= maxpods

---
+++ internal/render/hpa.go
@@ -0,0 +1,39 @@
+package render
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/derailed/tcell/v2"
+)
+
+// HorizontalPodAutoscaler renders a K8s HorizontalPodAutoscaler to screen.
+type HorizontalPodAutoscaler struct {
+	Generic
+}
+
+// ColorerFunc colors a resource row.
+func (hpa HorizontalPodAutoscaler) ColorerFunc() ColorerFunc {
+	return func(ns string, h Header, re RowEvent) tcell.Color {
+		c := DefaultColorer(ns, h, re)
+
+		maxPodsIndex := h.IndexOf("MAXPODS", true)
+		currentReplicasIndex := h.IndexOf("REPLICAS", true)
+		maxPodsS := strings.TrimSpace(re.Row.Fields[maxPodsIndex])
+		currentReplicasS := strings.TrimSpace(re.Row.Fields[currentReplicasIndex])
+
+		maxPods, err := strconv.Atoi(maxPodsS)
+		if err != nil {
+			return c
+		}
+		currentReplicas, err := strconv.Atoi(currentReplicasS)
+		if err != nil {
+			return c
+		}
+		if currentReplicas >= maxPods {
+			c = ErrColor
+			return c
+		}
+		return c
+	}
+}
