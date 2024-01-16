8480cc967868c1773798397f24cfbf3a6af52f48
Andrei Maltsev <amaltsev@alpha-sense.com>
2024-01-16 17:54:13 +0200

    adding arch to nodes view

--- internal/render/node_test.go
+++ internal/render/node_test.go
@@ -24,7 +24,7 @@ func TestNodeRender(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, "minikube", r.ID)
-	e := render.Fields{"minikube", "Ready", "master", "0", "v1.15.2", "4.15.0", "192.168.64.107", "<none>", "0", "10", "20", "0", "0", "4000", "7874"}
+	e := render.Fields{"minikube", "Ready", "master", "arm64", "0", "v1.15.2", "4.15.0", "192.168.64.107", "<none>", "0", "10", "20", "0", "0", "4000", "7874"}
 	assert.Equal(t, e, r.Fields[:15])
 }
 
