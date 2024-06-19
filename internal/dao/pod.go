6e21bfc119cbab5ad88f7573d6041d6aeb3695f6
jayson <jayson@laboys.org>
2024-06-19 16:24:25 +0800

    container logs should be escaped when printed

--- internal/dao/pod.go
+++ internal/dao/pod.go
@@ -16,6 +16,7 @@ import (
 	"github.com/derailed/k9s/internal/client"
 	"github.com/derailed/k9s/internal/render"
 	"github.com/derailed/k9s/internal/watch"
+	"github.com/derailed/tview"
 	"github.com/rs/zerolog/log"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -380,8 +381,8 @@ func readLogs(ctx context.Context, wg *sync.WaitGroup, stream io.ReadCloser, out
 	r := bufio.NewReader(stream)
 	for {
 		var item *LogItem
-		if bytes, err := r.ReadBytes('\n'); err == nil {
-			item = opts.ToLogItem(bytes)
+		if line, err := r.ReadString('\n'); err == nil {
+			item = opts.ToLogItem([]byte(tview.Escape(line)))
 		} else {
 			if errors.Is(err, io.EOF) {
 				e := fmt.Errorf("Stream closed %w for %s", err, opts.Info())
