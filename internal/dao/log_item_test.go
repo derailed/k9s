ba8d039d643907892461a1b113411684fc60c8a8
jayson <jayson@laboys.org>
2024-06-19 16:24:25 +0800

    container logs should be escaped when printed

--- internal/dao/log_item_test.go
+++ internal/dao/log_item_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/derailed/k9s/internal/client"
 	"github.com/derailed/k9s/internal/dao"
+	"github.com/derailed/tview"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -76,14 +77,24 @@ func TestLogItemRender(t *testing.T) {
 				ShowTimestamp:   false,
 			},
 			log: fmt.Sprintf("%s %s\n", "2018-12-14T10:36:43.326972-07:00", "2021-10-28T13:06:37Z [INFO] [blah-blah] Testing 1,2,3..."),
-			e:   "[yellow::]fred[-::] 2021-10-28T13:06:37Z [INFO] [blah-blah] Testing 1,2,3...\n",
+			e:   "[yellow::]fred[-::] 2021-10-28T13:06:37Z [INFO[] [blah-blah[] Testing 1,2,3...\n",
+		},
+		"escape": {
+			opts: dao.LogOptions{
+				Path:            "blee/fred",
+				Container:       "",
+				SingleContainer: false,
+				ShowTimestamp:   false,
+			},
+			log: fmt.Sprintf("%s %s\n", "2018-12-14T10:36:43.326972-07:00", `{"foo":["bar"]} Server listening on: [::]:5000`),
+			e:   `[yellow::]fred[-::] {"foo":["bar"[]} Server listening on: [::[]:5000` + "\n",
 		},
 	}
 
 	for k := range uu {
 		u := uu[k]
 		t.Run(k, func(t *testing.T) {
-			i := dao.NewLogItem([]byte(u.log))
+			i := dao.NewLogItem([]byte(tview.Escape(u.log)))
 			_, n := client.Namespaced(u.opts.Path)
 			i.Pod, i.Container = n, u.opts.Container
 
