86015601851bd7670b0ff2e9ca62ec03eebb88e1
Eric Bissonnette <ebisso.dev@gmail.com>
2024-08-28 11:26:27 -0400

    fix internal/render/hpa.go merge issue (#2856)

--- internal/render/hpa_test.go
+++ internal/render/hpa_test.go
@@ -6,57 +6,58 @@ package render
 import (
 	"testing"
 
+	"github.com/derailed/k9s/internal/model1"
 	"github.com/derailed/tcell/v2"
 	"github.com/derailed/tview"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHorizontalPodAutoscalerColorer(t *testing.T) {
-	hpaHeader := Header{
-		HeaderColumn{Name: "NAMESPACE"},
-		HeaderColumn{Name: "NAME"},
-		HeaderColumn{Name: "REFERENCE"},
-		HeaderColumn{Name: "TARGETS%"},
-		HeaderColumn{Name: "MINPODS", Align: tview.AlignRight},
-		HeaderColumn{Name: "MAXPODS", Align: tview.AlignRight},
-		HeaderColumn{Name: "REPLICAS", Align: tview.AlignRight},
-		HeaderColumn{Name: "AGE", Time: true},
+	hpaHeader := model1.Header{
+		model1.HeaderColumn{Name: "NAMESPACE"},
+		model1.HeaderColumn{Name: "NAME"},
+		model1.HeaderColumn{Name: "REFERENCE"},
+		model1.HeaderColumn{Name: "TARGETS%"},
+		model1.HeaderColumn{Name: "MINPODS", Align: tview.AlignRight},
+		model1.HeaderColumn{Name: "MAXPODS", Align: tview.AlignRight},
+		model1.HeaderColumn{Name: "REPLICAS", Align: tview.AlignRight},
+		model1.HeaderColumn{Name: "AGE", Time: true},
 	}
 
 	uu := map[string]struct {
-		h  Header
-		re RowEvent
+		h  model1.Header
+		re *model1.RowEvent
 		e  tcell.Color
 	}{
 		"when replicas = maxpods": {
 			h: hpaHeader,
-			re: RowEvent{
-				Kind: EventUnchanged,
-				Row: Row{
-					Fields: Fields{"blee", "fred", "fred", "100%", "1", "5", "5", "1d"},
+			re: &model1.RowEvent{
+				Kind: model1.EventUnchanged,
+				Row: model1.Row{
+					Fields: model1.Fields{"blee", "fred", "fred", "100%", "1", "5", "5", "1d"},
 				},
 			},
-			e: ErrColor,
+			e: model1.ErrColor,
 		},
 		"when replicas > maxpods, for some reason": {
 			h: hpaHeader,
-			re: RowEvent{
-				Kind: EventUnchanged,
-				Row: Row{
-					Fields: Fields{"blee", "fred", "fred", "100%", "1", "5", "6", "1d"},
+			re: &model1.RowEvent{
+				Kind: model1.EventUnchanged,
+				Row: model1.Row{
+					Fields: model1.Fields{"blee", "fred", "fred", "100%", "1", "5", "6", "1d"},
 				},
 			},
-			e: ErrColor,
+			e: model1.ErrColor,
 		},
 		"when replicas < maxpods": {
 			h: hpaHeader,
-			re: RowEvent{
-				Kind: EventUnchanged,
-				Row: Row{
-					Fields: Fields{"blee", "fred", "fred", "100%", "1", "5", "1", "1d"},
+			re: &model1.RowEvent{
+				Kind: model1.EventUnchanged,
+				Row: model1.Row{
+					Fields: model1.Fields{"blee", "fred", "fred", "100%", "1", "5", "1", "1d"},
 				},
 			},
-			e: StdColor,
+			e: model1.StdColor,
 		},
 	}
 
