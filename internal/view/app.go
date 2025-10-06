944e409c3916a0ebb6f608aa71a51a03a3fd2f50
derailed <fernand.galiana@gmail.com>
2025-10-06 16:58:17 -0600

    fix#3594-yaml busted

--- internal/view/app.go
+++ internal/view/app.go
@@ -626,9 +626,9 @@ func (a *App) toggleCrumbsCmd(evt *tcell.EventKey) *tcell.EventKey {
 }
 
 func (a *App) gotoCmd(evt *tcell.EventKey) *tcell.EventKey {
-	cmd := a.GetCmd()
-	if a.CmdBuff().IsActive() && cmd != "" {
-		a.gotoResource(cmd, "", true, true)
+	c := a.GetCmd()
+	if a.CmdBuff().IsActive() && c != "" {
+		a.gotoResource(c, "", true, true)
 		a.ResetCmd()
 		return nil
 	}
