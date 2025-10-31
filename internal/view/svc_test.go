fd5481aa27447f1e1dedb2e7fed2237806e03fde
Ümüt Özalp <umut@ozalp.dk>
2025-10-31 22:52:33 +0100

    test: update expected hints count in TestServiceNew

--- internal/view/svc_test.go
+++ internal/view/svc_test.go
@@ -174,5 +174,5 @@ func TestServiceNew(t *testing.T) {
 
 	require.NoError(t, s.Init(makeCtx(t)))
 	assert.Equal(t, "Services", s.Name())
-	assert.Len(t, s.Hints(), 12)
+	assert.Len(t, s.Hints(), 13)
 }
