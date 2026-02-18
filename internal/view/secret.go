9155cfc41526e5c08a17dd5b86a2bd060e1eac43
Kyle Brown <kyle.brown@getweave.com>
2026-02-18 16:10:29 -0700

    feat: decode secrets during edit mode

--- internal/view/secret.go
+++ internal/view/secret.go
@@ -4,6 +4,9 @@
 package view
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/derailed/k9s/internal/client"
 	"github.com/derailed/k9s/internal/config/data"
 	"github.com/derailed/k9s/internal/dao"
@@ -27,11 +30,39 @@ func NewSecret(gvr *client.GVR) ResourceViewer {
 	return &s
 }
 
+func (s *Secret) bindDangerousKeys(aa *ui.KeyActions) {
+	aa.Bulk(ui.KeyMap{
+		ui.KeyE: ui.NewKeyActionWithOpts("Edit", s.editCmd, ui.ActionOpts{
+			Visible:   true,
+			Dangerous: true,
+		}),
+	})
+}
+
 func (s *Secret) bindKeys(aa *ui.KeyActions) {
+	if !s.App().Config.IsReadOnly() {
+		s.bindDangerousKeys(aa)
+	}
+
 	aa.Bulk(ui.KeyMap{
 		ui.KeyX: ui.NewKeyAction("Decode", s.decodeCmd, true),
 		ui.KeyU: ui.NewKeyAction("UsedBy", s.refCmd, true),
 	})
+}
+
+func (s *Secret) editCmd(evt *tcell.EventKey) *tcell.EventKey {
+	path := s.GetTable().GetSelectedItem()
+	if path == "" {
+		return evt
+	}
+
+	s.Stop()
+	defer s.Start()
+	if err := editSecretRes(s.App(), s.GVR(), path); err != nil {
+		s.App().Flash().Err(err)
+	}
+
+	return nil
 }
 
 func (s *Secret) refCmd(evt *tcell.EventKey) *tcell.EventKey {
@@ -68,4 +99,57 @@ func (s *Secret) decodeCmd(evt *tcell.EventKey) *tcell.EventKey {
 	}
 
 	return nil
+}
+
+func editSecretRes(app *App, gvr *client.GVR, path string) error {
+	if path == "" {
+		return fmt.Errorf("nothing selected %q", path)
+	}
+	ns, n := client.Namespaced(path)
+	if n == "" {
+		return fmt.Errorf("missing resource name in path %q", path)
+	}
+	if ok, err := app.Conn().CanI(ns, gvr, n, client.PatchAccess); !ok || err != nil {
+		return fmt.Errorf("current user can't edit resource %s", gvr)
+	}
+
+	res, err := dao.AccessorFor(app.factory, gvr)
+	if err != nil {
+		return fmt.Errorf("failed to get accessor: %w", err)
+	}
+	sec, ok := res.(*dao.Secret)
+	if !ok {
+		return fmt.Errorf("expecting Secret DAO but got %T", res)
+	}
+
+	yaml, err := sec.EditYAML(path)
+	if err != nil {
+		return fmt.Errorf("failed to get secret YAML: %w", err)
+	}
+
+	tmpFile, err := os.CreateTemp("", "k9s-secret-*.yaml")
+	if err != nil {
+		return fmt.Errorf("failed to create temp file: %w", err)
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if _, err := tmpFile.WriteString(yaml); err != nil {
+		tmpFile.Close()
+		return fmt.Errorf("failed to write temp file: %w", err)
+	}
+	if err := tmpFile.Close(); err != nil {
+		return fmt.Errorf("failed to close temp file: %w", err)
+	}
+
+	if !edit(app, &shellOpts{clear: true, args: []string{tmpFile.Name()}}) {
+		return fmt.Errorf("failed to launch editor")
+	}
+
+	args := make([]string, 0, 5)
+	args = append(args, "replace", "-f", tmpFile.Name())
+	if ns != client.BlankNamespace {
+		args = append(args, "-n", ns)
+	}
+
+	return runK(app, &shellOpts{clear: false, args: args})
 }
