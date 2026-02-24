a8833d0c52e853ad03cd327fbfe76fd0e35b6116
Máté Eckl <eckl.mate@alerant.hu>
2026-02-24 07:08:20 +0100

    fix: Allow whitespaces in context with @ctx notation

--- internal/view/cmd/interpreter_test.go
+++ internal/view/cmd/interpreter_test.go
@@ -650,6 +650,31 @@ func TestArgs(t *testing.T) {
 			ok:  true,
 			ctx: "fred",
 		},
+		"with-space-context-unquoted-last": {
+			cmd: "po @context with space",
+			ok:  true,
+			ctx: "context",
+		},
+		"with-space-context-unquoted-with-slashes": {
+			cmd: "po @default/server:6443/Firstname Middlename Lastname",
+			ok:  true,
+			ctx: "default/server:6443/Firstname",
+		},
+		"with-space-context-quoted": {
+			cmd: "po @'context with space'",
+			ok:  true,
+			ctx: "context with space",
+		},
+		"with-space-context-quoted-then-namespace": {
+			cmd: "po @'context with space' fred",
+			ok:  true,
+			ctx: "context with space",
+		},
+		"with-space-context-quoted-double-then-namespace": {
+			cmd: "po @\"context with space\" fred",
+			ok:  true,
+			ctx: "context with space",
+		},
 	}
 
 	for k := range uu {
