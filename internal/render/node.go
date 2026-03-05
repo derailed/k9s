73e47916035d3fc45dde98f5835beabe73946a51
Michael <229557213@qq.com>
2026-03-05 11:14:05 +0800

    show NotReady nodes in red even when cordoned
    feat(node): show NotReady nodes in red even when cordoned

--- internal/render/node.go
+++ internal/render/node.go
@@ -192,12 +192,13 @@ func (Node) diagnose(ss []string) error {
 	}
 
 	var ready bool
+	var hasSchedulingDisabled bool
 	for _, s := range ss {
 		if s == "" {
 			continue
 		}
 		if s == "SchedulingDisabled" {
-			return cordonErr
+			hasSchedulingDisabled = true
 		}
 		if s == "Ready" {
 			ready = true
@@ -208,6 +209,10 @@ func (Node) diagnose(ss []string) error {
 		return notReadyErr
 	}
 
+	if hasSchedulingDisabled {
+		return cordonErr
+	}
+	
 	return nil
 }
 
