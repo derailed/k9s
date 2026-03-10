f26416746b57dd985f41e10809a20a53942399a5
Arsen Muradyan <arsenmuradyan604@gmail.com>
2026-03-10 16:58:47 +0400

    moved into single variable

--- internal/view/table.go
+++ internal/view/table.go
@@ -147,18 +147,9 @@ func (t *Table) defaultEnv() Env {
 	env["RESOURCE_VERSION"] = t.GVR().V()
 	env["RESOURCE_NAME"] = t.GVR().R()
 
-	// Add list of names and namespaces in case of multiple selected items
+	// Add list of selected FQNs
 	paths := t.GetSelectedItems()
-	namespaces := []string{}
-	names := []string{}
-	for _, path := range paths {
-		namespace, name := client.Namespaced(path)
-		names = append(names, name)
-		namespaces = append(namespaces, namespace)
-	}
-	env["NAMES"] = strings.Join(names, ",")
-	env["NAMESPACES"] = strings.Join(namespaces, ",")
-
+	env["SELECTED_FQNS"] = strings.Join(paths, ",")
 	return env
 }
 
