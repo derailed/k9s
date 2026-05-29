a02c6e0dfd8246ee24c92a33b02790734b4cab9a
vishalvinayram5432@gmail.com <vishalvinayram5432@gmail.com>
2026-05-30 00:22:00 +0530

    adding the ClearMarks() after setNamespace()

--- internal/view/browser.go
+++ internal/view/browser.go
@@ -598,6 +598,7 @@ func (b *Browser) setNamespace(ns string) {
 		ns = client.ClusterScope
 	}
 	b.GetModel().SetNamespace(ns)
+	b.GetTable().ClearMarks()
 }
 
 func (b *Browser) defaultContext() context.Context {
